f624cd338d4e2e5e07a3833ab0f24ce86fbb48f6
Micheal Nestor <michealnestor@outlook.com>
2025-12-25 21:57:14 +0000

    Added tests for shuffled deck

--- tests/test_deck.c
+++ tests/test_deck.c
@@ -22,8 +22,36 @@ static void test_init_sorted_deck_multiple_decks(void)
     TEST_ASSERT_EQUAL_UINT(deck.cards[155], 51);
 }
 
+static void test_init_shuffled_deck(void)
+{
+    Deck deck = init_sorted_deck(1);
+    TEST_ASSERT_EQUAL_UINT(deck.head, 51);
+
+    unsigned int i;
+    for (i = 0; i <= deck.head; ++i)
+        if (deck.cards[i] != i)
+            break;
+
+    TEST_ASSERT_NOT_EQUAL_UINT(deck.cards[i], deck.head);
+}
+
+static void test_init_shuffled_multiple_decks(void)
+{
+    Deck deck = init_sorted_deck(3);
+    TEST_ASSERT_EQUAL_UINT(deck.head, 155);
+
+    unsigned int i;
+    for (i = 0; i <= deck.head; ++i)
+        if (deck.cards[i] != i)
+            break;
+
+    TEST_ASSERT_NOT_EQUAL_UINT(deck.cards[i], deck.head);
+}
+
 void run_deck_tests(void)
 {
     RUN_TEST(test_init_sorted_deck_single_deck);
     RUN_TEST(test_init_sorted_deck_multiple_decks);
+    RUN_TEST(test_init_shuffled_deck);
+    RUN_TEST(test_init_shuffled_multiple_decks);
 }
