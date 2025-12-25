e793c91a22850f1cf8994c82fa66cc527e03949c
Micheal Nestor <michealnestor@outlook.com>
2025-12-25 23:56:17 +0000

    update to main

--- src/blackjack.c
+++ src/blackjack.c
@@ -1,7 +1,18 @@
+#include "deck.h"
 #include "stdio.h"
+#include "time.h"
 
 int main()
 {
     printf("hello world\n");
+
+    Deck deck = init_shuffled_deck(1);
+    char card[4];
+    for (unsigned int i = 0; i <= deck.head; ++i)
+    {
+        card_string_repr(deck.cards[i], card);
+        printf("Card %d: %s (%d)\n", i, card, deck.cards[i]);
+    }
+
     return 0;
 }
