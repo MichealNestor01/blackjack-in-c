42f897c13f3dbc534f42a5aec967d808b15efd03
Micheal Nestor <michealnestor@outlook.com>
2025-12-26 00:44:15 +0000

    added configuration through command line arguments

--- src/blackjack.c
+++ src/blackjack.c
@@ -1,10 +1,29 @@
-#include "deck.h"
 #include "stdio.h"
+#include "stdlib.h"
+#include "deck.h"
 #include "time.h"
 
-int main()
+int main(int argc, char **argv)
 {
-    printf("hello world\n");
+    if (argc < 3)
+    {
+        printf("USAGE: %s players decks\n", argv[0]);
+        exit(0);
+    }
+
+    int players = atoi(argv[1]);
+    if (players < 1 || players > 10)
+    {
+        printf("USAGE: must select between 1 and 10 players\n");
+        exit(0);
+    }
+
+    int decks = atoi(argv[2]);
+    if (decks < 1 || decks > 8)
+    {
+        printf("USAGE: must select between 1 and 8 decks to play with\n");
+        exit(0);
+    }
 
     Deck deck = init_shuffled_deck(1);
     char card[4];
