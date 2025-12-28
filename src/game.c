44b49ec09b060b88f1739af1df4bb267cf912cbb
Micheal Nestor <michealnestor@outlook.com>
2025-12-28 20:08:33 +0000

    added the dealer's turn

--- src/game.c
+++ src/game.c
@@ -22,26 +22,29 @@ void play_game(unsigned short player_count, unsigned short deck_count)
 
 void play_round(Player *players, unsigned short player_count, Deck *deck)
 {
+    Player *dealer = &players[8];
+
     // deal initial two cards
     printf("== Dealing Cards ==\n");
     for (unsigned short card = 0; card < 2; ++card)
     {
         for (unsigned short player_index = 0; player_index < player_count; ++player_index)
             deal_card(deck, &players[player_index]);
-        deal_card(deck, &players[8]); // dealer
+        deal_card(deck, dealer); // dealer
         display_hands(players, player_count, 1);
     }
 
     // peak for dealer blackjack if possible
-    unsigned short dealer_first_card_val = players[8].hand[0] % 13;
+    unsigned short dealer_first_card_val = dealer->hand[0] % 13;
     if (dealer_first_card_val == 0 || dealer_first_card_val > 9)
     {
         printf("== Peaking at the dealer's hand ==\n");
-        if (players[8].hand_score == 21)
+        if (dealer->hand_score == 21)
         {
             printf("The dealer has blackjack, round over\n");
             for (unsigned short player_index = 0; player_index < player_count; ++player_index)
-                udpdate_score(&players[player_index], 21);
+                score_player(&players[player_index], dealer);
+            dealer->hand_head = -1;
             return;
         }
         printf("The dealer does not have blackjack\n");
@@ -54,14 +57,18 @@ void play_round(Player *players, unsigned short player_count, Deck *deck)
     for (unsigned short player_index = 0; player_index < player_count; ++player_index)
     {
         unsigned short player_number = player_index + 1;
-        hand_string_repr(&players[player_index], buff);
-        printf("Player %hu turn! Current hand: %s (%hu).\n", player_number, buff, players[player_index].hand_score);
+        Player *player = &players[player_index];
+        hand_string_repr(player, buff);
+        printf("Player %hu turn! Current hand: %s (%hu).\n", player_number, buff, player->hand_score);
         while (1) // turn loop
         {
             // check for blackjack
-            if (players[player_index].hand_score == 21)
+            if (player->hand_score == 21)
             {
-                printf("Player %hu has blackjack, turn over.\n", player_number);
+                if (player->hand_head == 1)
+                    printf("Player %hu has blackjack, turn complete.\n", player_number);
+                else
+                    printf("Player %hu has 21, turn complete.\n", player_number);
                 player_not_bust = 1;
                 break;
             }
@@ -70,25 +77,25 @@ void play_round(Player *players, unsigned short player_count, Deck *deck)
             unsigned short choice = get_player_choice(player_number);
             if (choice == 1)
             {
-                deal_card(deck, &players[player_index]);
-                hand_string_repr(&players[player_index], buff);
-                printf("!HIT! Updated hand: %s (%hu)\n", buff, players[player_index].hand_score);
+                deal_card(deck, player);
+                hand_string_repr(player, buff);
+                printf("!HIT! Updated hand: %s (%hu)\n", buff, player->hand_score);
             }
             else if (choice == 2)
             {
-                printf("!STICK! Final hand: %s (%hu)\n", buff, players[player_index].hand_score);
+                printf("!STICK! Final hand: %s (%hu)\n", buff, player->hand_score);
                 player_not_bust = 1;
                 break;
             }
 
             // bust check
-            if (players[player_index].hand_score > 21)
+            if (player->hand_score > 21)
             {
                 printf("!BUST! Player %hu's turn is over.\n", player_number);
                 break;
             }
         }
-        deal_card(deck, &players[player_index]);
+        deal_card(deck, player);
     }
 
     // check if all players are bust
@@ -96,11 +103,29 @@ void play_round(Player *players, unsigned short player_count, Deck *deck)
     {
         printf("All players are BUST, round complete! All players loose 1 point\n");
         for (unsigned short player_index = 0; player_index < player_count; ++player_index)
-            udpdate_score(&players[player_index], players[8].hand_score);
+            score_player(&players[player_index], dealer);
         return;
     }
 
     // dealers turn
+    printf("== Dealer's turn ==\n");
+    while (1)
+    {
+        hand_string_repr(dealer, buff);
+        printf("Dealer's hand: %s (%hu)\n", buff, dealer->hand_score);
+        if (dealer->hand_score > 16)
+        {
+            printf("!STAND! The dealer's turn is complete\n");
+            break;
+        }
+        else if (dealer->hand_score > 21)
+        {
+            printf("!BUST! The dealer's turn is complete\n");
+            break;
+        }
+        printf("!HIT! Dealing the dealer another card\n");
+        deal_card(deck, dealer);
+    }
 }
 
 void deal_card(Deck *deck, Player *player)
